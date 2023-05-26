c8dcaea77fe7ecbe9239a71474d9e315ee712d01
abel-tefera <abeltefera16@gmail.com>
2023-05-26 13:54:36 +0300

    Added tests for wanted component

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,23 +1,5 @@
 const mockData = {
-  aliases: [
-    'Abd Al-Rauf Bin Al-Habib Bin Yousef Al-Jiddi',
-    'Abderraouf Dey',
-    'A. Raouf Jdey',
-    "Abdal Ra'Of Bin Muhammed Bin Yousef Al-Jadi",
-    'Farouq Al-Tunisi',
-    'Abderraouf Ben Habib Jeday',
-  ],
-  age_max: null,
-  age_min: null,
-  age_range: null,
-  complexion: 'Olive',
-  caution: null,
-  coordinates: [],
   dates_of_birth_used: ['May 30, 1965'],
-  details:
-    '<p>Abderraouf Jdey is wanted for questioning in connection with possible terrorist threats against the United States.</p>',
-  eyes: 'brown',
-  hair: 'brown',
   images: [
     {
       original:
@@ -28,40 +10,14 @@ const mockData = {
       thumb:
         'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/@@images/image/thumb',
     },
-    {
-      original:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey4.jpg',
-      large:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey4.jpg/@@images/image/large',
-      caption: '',
-      thumb:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey4.jpg/@@images/image/thumb',
-    },
-    {
-      original:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey.jpg',
-      large:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey.jpg/@@images/image/large',
-      caption:
-        'FBI Laboratory Photographic Retouch. Jdey is wanted for questioning in connection with possible terrorist threats against the United States.',
-      thumb:
-        'https://www.fbi.gov/wanted/terrorinfo/abderraouf-jdey/jdey.jpg/@@images/image/thumb',
-    },
   ],
-  height_max: 72,
-  height_min: 72,
-  nationality: null,
-  occupations: null,
-  place_of_birth: 'Tunisia',
   reward_max: 4999999,
   reward_min: 1000000,
   reward_text:
-    'The Rewards For Justice Program, United States Department of State, is offering a reward of up to $5 million for information leading directly to the capture of Abderraouf Jdey.',
-  sex: 'Male',
+    'The Rewards For Justice Program, United States Department of State, is offering a reward of up to $5 million for information leading directly to the capture of ...',
   subjects: ['Seeking Information - Terrorism'],
-  title: 'ABDERRAOUF JDEY',
+  title: 'PERSON 1',
   warning_message: 'SHOULD BE CONSIDERED ARMED AND DANGEROUS',
-  weight: '209 pounds',
   wanted_id: '8afa8ac1eccbfea19b9d4d9682145b14',
   description: '',
 };
